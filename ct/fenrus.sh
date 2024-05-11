050f77fa8fd49d83a2310cda589e6d4dc6eb168e
Xiathier Pendragon <rocky-dragon@outlook.com>
2024-05-12 01:45:41 +0200

    oop

--- ct/fenrus.sh
+++ ct/fenrus.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/tteck/xpdg/main/misc/build.func)
+source <(curl -s https://raw.githubusercontent.com/xpdg/Proxmox/main/misc/build.func)
 # Copyright (c) 2021-2024 tteck
 # Author: tteck (tteckster)
 # Co-Author: Scorpoon
--- ct/mediamtx.sh
+++ ct/mediamtx.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-source <(curl -s https://raw.githubusercontent.com/tteck/xpdg/main/misc/build.func)
+source <(curl -s https://raw.githubusercontent.com/xpdg/Proxmox/main/misc/build.func)
 # Copyright (c) 2021-2024 tteck
 # Author: tteck (tteckster)
 # License: MIT
